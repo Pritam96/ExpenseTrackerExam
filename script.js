68ef86d211be0ea6e6e2b8df6efa05686d46dec9
Pritam96 <pritammondal96@gmail.com>
2023-05-17 16:01:41 +0530

    code improvements

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://crudcrud.com/api/ab8754e96ebc414880616b900da507c9';
+const BASE_URL = 'https://crudcrud.com/api/ff42e11401de4cd6ad2d7205c47978b4';
 
 const form = document.querySelector('#_form');
 
@@ -15,72 +15,85 @@ function addToTheList(e) {
 
   axios
     .post(`${BASE_URL}/stock`, item)
-    .then((res) => console.log(res))
+    .then((res) => {
+      console.log(res);
+      showAll();
+    })
     .catch((err) => console.log(err));
 
   form.reset();
-  show();
 }
 
-function products(item) {
-  let div_card = document.createElement('div');
-  div_card.className = 'card';
+function product(item) {
+  let card = document.createElement('div');
+  card.className = 'card';
 
-  let div_card_body = document.createElement('div');
-  div_card_body.className = 'card-body';
+  let card_body = document.createElement('div');
+  card_body.className = 'card-body';
 
-  let div_row = document.createElement('div');
-  div_row.className = 'row';
+  let row = document.createElement('div');
+  row.className = 'row';
 
-  let div_col_price = document.createElement('div');
-  div_col_price.className = 'col';
-  div_col_price.appendChild(document.createTextNode(`${item.price} Rupees`));
+  let price_col = document.createElement('div');
+  price_col.className = 'col';
+  price_col.appendChild(document.createTextNode(`${item.price} Rupees`));
 
-  let div_col_name = document.createElement('div');
-  div_col_name.className = 'col';
-  div_col_name.appendChild(document.createTextNode(`${item.name}`));
+  let name_col = document.createElement('div');
+  name_col.className = 'col';
+  name_col.appendChild(document.createTextNode(`${item.name}`));
 
   // Delete Button
   let deleteBtn = document.createElement('div');
   deleteBtn.className = 'col';
-  let btn1 = document.createElement('button');
-  btn1.id = 'get_one';
-  btn1.className = 'btn btn-outline-secondary';
-  btn1.appendChild(document.createTextNode('Delete Product'));
-  deleteBtn.appendChild(btn1);
+  let button = document.createElement('button');
+  button.className = 'btn btn-outline-secondary';
+  button.appendChild(document.createTextNode('Delete Product'));
+  deleteBtn.appendChild(button);
 
-  btn1.onclick = () => {
+  button.onclick = () => {
     axios
       .delete(`${BASE_URL}/stock/${item._id}`)
-      .then((res) => console.log(res))
+      .then((res) => {
+        console.log(res);
+        showAll();
+      })
       .catch((err) => console.log(err));
-
-    show();
   };
 
-  div_row.appendChild(div_col_price);
-  div_row.appendChild(div_col_name);
-  div_row.appendChild(deleteBtn);
+  row.appendChild(price_col);
+  row.appendChild(name_col);
+  row.appendChild(deleteBtn);
 
-  div_card_body.appendChild(div_row);
-  div_card.appendChild(div_card_body);
+  card_body.appendChild(row);
+  card.appendChild(card_body);
 
-  document.querySelector('#response').appendChild(div_card);
+  document.querySelector('#response').appendChild(card);
   document.querySelector(
     '#total'
-  ).innerHTML = `Total value worth of products: Rs ${total}`;
+  ).innerHTML = `<h5>Total value worth of products: Rs ${total}</h5>`;
 }
 
-function show() {
-  total = 0;
+function showAll() {
   document.querySelector('#response').innerHTML = '';
-  axios.get(`${BASE_URL}/stock`).then((res) => {
-    res.data.forEach((item) => {
-      total += item.price;
-      products(item);
-      console.log(item);
-    });
-  });
+  total = 0;
+  axios
+    .get(`${BASE_URL}/stock`)
+    .then((res) => {
+      if (res.data.length < 1) {
+        console.log('no data');
+        total = 0;
+        document.querySelector(
+          '#total'
+        ).innerHTML = `<h5>Total value worth of products: Rs ${total}</h5>`;
+      } else {
+        res.data.forEach((item) => {
+          total += item.price;
+          product(item);
+          console.log(item);
+        });
+      }
+    })
+    .catch((err) => console.log(err));
 }
 
-show();
+showAll();
